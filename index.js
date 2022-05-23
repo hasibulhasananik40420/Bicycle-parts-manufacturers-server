d71417d1d1c7ae8e34659cf8ba62a86a18d9c2a4
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-05-23 14:28:16 +0600

    make addmin done

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.uffti.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+ 
+function verifyJWT(req,res,next){
+  const authHeader  = req.headers.authorization 
+  if(!authHeader){
+    return res.status(401).send({message:'Unauthrized access'})
+  }
+  const token = authHeader.split(' ')[1]
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded) {
+    if(err){
+      return res.status(403).send({message:'Forbiden access'})
+    }
+   req.decoded = decoded
+   next()
+  });
+}
+
 
 async function run() {
 
@@ -24,6 +41,20 @@ async function run() {
       const reviewCollection = client.db("users").collection("review")
       const userCollection = client.db("users").collection("user")
       console.log('db cnnected');
+
+       
+    //   const verifyAdmin =async(req,res,next)=>{
+    //     const requester = req.decoded.email
+    //     const requesterAccount= await userCollection.findOne({email:requester})
+    //     if(requesterAccount.role === 'admin'){
+    //       next()
+    //     }
+    //     else{
+    //      res.status(403).send({message:'Forbiden access'})
+    //     }
+    // }
+
+
 
       //all products
       app.get('/products' , async(req,res)=>{
@@ -65,9 +96,45 @@ async function run() {
             $set: user
           };
           const result = await userCollection.updateOne(filter, updateDoc, options);
-          res.send(result)
+          const token = jwt.sign({email:email} , process.env.ACCESS_TOKEN ,  { expiresIn: '1h' })
+          res.send({result, token})
     })
-    
+    //load all users
+    app.get('/user' , async(req,res)=>{
+      const users = await userCollection.find().toArray()
+      res.send(users)
+    })
+
+
+    //admin get
+    app.get('/admin/:email',async(req,res)=>{
+      const email = req.params.email 
+      const user = await userCollection.findOne({email:email}) 
+      const isAdmin = user.role === 'admin'
+     res.send({admin:isAdmin})
+    })
+//admin role
+
+app.put('/user/admin/:email',verifyJWT, async(req,res)=>{
+  const email = req.params.email 
+  const requester = req.decoded.email 
+  const requesterAccount = await userCollection.findOne({email:requester})  
+  if(requesterAccount.role === 'admin' ){
+      const filter ={email: email} 
+      const options = { upsert: true };
+      const updateDoc = {
+          $set: {role:'admin'}
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result)
+  }
+   else{
+     res.status(403).send({message: 'forbiden access'})
+   }
+ 
+})
+
+
   
     } 
     
